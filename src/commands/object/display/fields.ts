a3f0019cfc4046a99777b88227fa881fd92fff64
Tom Carman <tom.carman@gmail.com>
2023-10-02 11:50:04 +0100

    refactor: tidy up structure of 'object display field' command

--- src/commands/object/display/fields.ts
+++ src/commands/object/display/fields.ts
@@ -1,15 +1,14 @@
 import { Ux, SfCommand, Flags } from '@salesforce/sf-plugins-core';
 import { Messages } from '@salesforce/core';
-import { AnyJson } from '@salesforce/ts-types';
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('sf-raven', 'object.display.fields');
 
-// export type ObjectDisplayFieldsResult = {
-//   path: string;
-// };
+export type ObjectDisplayFieldsResult = {
+  result: object;
+};
 
-export default class ObjectDisplayFields extends SfCommand<AnyJson> {
+export default class ObjectDisplayFields extends SfCommand<ObjectDisplayFieldsResult> {
   public static readonly summary = messages.getMessage('summary');
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
@@ -27,9 +26,7 @@ export default class ObjectDisplayFields extends SfCommand<AnyJson> {
     }),
   };
 
-  public async run(): Promise<AnyJson> {
-    const ux = new Ux({ jsonEnabled: this.jsonEnabled() });
-
+  public async run(): Promise<ObjectDisplayFieldsResult> {
     this.spinner.start('Loading...');
 
     interface QueryResult {
@@ -47,23 +44,16 @@ export default class ObjectDisplayFields extends SfCommand<AnyJson> {
     }
 
     const { flags } = await this.parse(ObjectDisplayFields);
+    const ux = new Ux({ jsonEnabled: this.jsonEnabled() });
 
     const org = flags['target-org'];
     const conn = org.getConnection();
     const query = `SELECT Label, QualifiedApiName, DataType FROM FieldDefinition WHERE EntityDefinition.QualifiedApiName = '${flags.sobject}' ORDER BY QualifiedApiName`;
-
     const result = (await conn.query(query)) as QueryResult;
 
     this.spinner.stop();
 
-    // const data = result.records.map((record: Record) => ({
-    //     label: record.Label,
-    //     qualifiedApiName: record.QualifiedApiName,
-    //     dataType: record.DataType
-    //   }));
-
     // Return table of fields
-
     ux.table(result.records, {
       label: {
         header: 'Name',
@@ -83,7 +73,6 @@ export default class ObjectDisplayFields extends SfCommand<AnyJson> {
     ux.log(`\n${conn.instanceUrl}/lightning/setup/ObjectManager/${flags.sobject}/RecordTypes/view`);
 
     // Return an object to be displayed with --json
-    // const outputString = JSON.stringify(result.records);
-    return result as unknown as AnyJson;
+    return result as unknown as ObjectDisplayFieldsResult;
   }
 }
